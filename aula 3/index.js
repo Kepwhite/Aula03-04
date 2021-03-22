66c0e0c353a1034ad3d9682f0584c8cfb211f360
Kepwhite <80289790+Kepwhite@users.noreply.github.com>
2021-03-22 18:58:12 -0400

    Add files via upload

---
+++ aula 3/index.js
@@ -0,0 +1,12 @@
+$(document).ready(
+    function () {
+        alert("rakan falou fiu fiu")
+    }
+)
+
+$("#bt_inicio").click(
+    function(){
+        alert("Opa, Você foi enganado pelo Deus Usopp")
+    }
+)
+
